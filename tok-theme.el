cb4be2790983489529dc533d6941e02b02791aef
Topi Kettunen <topi@topikettunen.com>
2022-11-22 08:52:51 +0100

    Add autoload

--- tok-theme.el
+++ tok-theme.el
@@ -119,6 +119,11 @@
    `(markdown-header-face ((t (nil))))
    `(markdown-header-delimiter-face ((t (nil))))))
 
+;;;###autoload
+(when (and (boundp 'custom-theme-load-path) load-file-name)
+  (add-to-list 'custom-theme-load-path
+               (file-name-as-directory (file-name-directory load-file-name))))
+
 (provide-theme 'tok)
 
 ;; Local Variables:
